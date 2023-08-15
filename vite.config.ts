c1e91efd2ae21ce5e7c5f9770af1a440ad35779f
nimixx <nimixx.dev@proton.me>
2023-08-16 01:03:58 +0200

    Add base repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/<REPO>/',
+    base: 'accordion-fm',
   }
 
   if (command !== 'serve') {
